ec0fef8c3a57de6fed9114f801b125003358e114
Ngahu <jamaalaraheem@gmail.com>
2016-08-16 22:52:57 +0300

    the adding of the made dictionary in the .py file

--- clasesBasics.py
+++ clasesBasics.py
@@ -1,14 +1,2 @@
-class Hero:
-    def _init_(self,name):
-        self.name = name
-        self.health = 100
-    def eat(self,food):
-        if (food == 'apple'):
-            self.health -=100
-        elif (food == 'pizza'):
-            self.health +=20
-
-print Bob.name
-print Bob.health
-Bob.eat('apple')
-print Bob.health
+menbers = {"dad":32,"mum":30,"siz":49,"bro":42}
+print "hehe"
