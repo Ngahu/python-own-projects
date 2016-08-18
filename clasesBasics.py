42509e8eedab7ab77efc5c979f7efd284af31bdf
Ngahu <jamaalaraheem@gmail.com>
2016-08-18 23:45:47 +0300

    adding of the modified classes

--- clasesBasics.py
+++ clasesBasics.py
@@ -16,17 +16,4 @@ while True:
         
         
         
-        
-        def prime(number):
-    for i in range(2,number,1):
-        if number % i == 0:
-            return False
-
-    return True
-entry = int(input("Please enter the number: "))
-while True:
-    if prime(entry):
-        print ("It's a prime number. ")
-    else:
-        print ("It's not a prime number.. ")
-        break
+ 
