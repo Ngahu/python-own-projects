f3ee640e2a5b47c151894c9e813f104e8f67a6be
Ngahu <jamaalaraheem@gmail.com>
2016-08-18 23:47:30 +0300

    adding of the args and kwargs functions

--- exercise.py
+++ exercise.py
@@ -106,3 +106,327 @@ def func_with_both(one, two, *args):
     says that it can take just two arguments or any number of arguments as long
     as there are at least two.
     """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
+def func_with_two(one, two):
+    """
+    This function only takes in two arguments because we explicitly
+    defined two formal parameters. Any more or any less will cause an
+    error.
+    """
+
+def func_with_start_args(*args):
+    """
+    This function can take in any number of arguments, including zero!
+    """
+
+def func_with_both(one, two, *args):
+    """
+    This function requires <i>at least</i> two arguments. The *args at the end
+    says that it can take just two arguments or any number of arguments as long
+    as there are at least two.
+    """
