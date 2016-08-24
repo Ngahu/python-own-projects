fa25af0589e1c11bccc389cc57ef4fc601fbf601
Ngahu <jamaalaraheem@gmail.com>
2016-08-24 15:14:47 +0300

    adding of the class

--- clasesBasics.py
+++ clasesBasics.py
@@ -1,19 +1,4 @@
-def prime(number):
-    for i in range(2,number,1):
-        if number % i == 0:
-            return False
-
-    return True
-entry = int(input("Please enter the number: "))
-while True:
-    if prime(entry):
-        print ("It's a prime number. ")
-        continue
-    else:
-        print ("It's not a prime number.. ")
-        continue
-
-        
-        
-        
- 
+def sumProblems(x,y):
+    sum=x+y
+    sentence = raw_input('please input value here')
+    print(sentence)
