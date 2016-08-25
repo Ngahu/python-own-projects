b1d41a0b460950394d3802c006706fab5580851c
Ngahu <jamaalaraheem@gmail.com>
2016-08-25 16:54:35 +0300

    adding of the flask trial

--- classes.py
+++ classes.py
@@ -1,33 +1,40 @@
-rom
-datetime
-import datetime
+@app.route('/')
+def show_posts():
+    posts = []
 
-from flask import (
-    Flask,
-    abort,
-    flash,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
-from flask.ext.stormpath import (
-    StormpathError,
-    StormpathManager,
-    User,
-    login_required,
-    login_user,
-    logout_user,
-    user,
-)
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'some_really_long_random_string_here'
-app.config['STORMPATH_API_KEY_FILE'] = 'apiKey.properties'
-app.config['STORMPATH_APPLICATION'] = 'flaskr'
+for account in stormpath_manager.application.accounts:
+    if account.custom_data.get('posts'):
+        posts.extend(account.custom_data['posts'])
 
-stormpath_manager = StormpathManager(app)
+posts = sorted(posts, key=lambda k: k['date'], reverse=True)
+return render_template('show_posts.html', posts=posts)
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add_post():
+    if not user.custom_data.get('posts'):
+        user.custom_data['posts'] = []
+
+
+user.custom_data['posts'].append({
+    'date': datetime.utcnow().isoformat(),
+    'title': request.form['title'],
+    'text': request.form['text'],
+})
+user.save()
+
+flash('New post successfully added.')
+return redirect(url_for('show_posts'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+
+
+if request.method == 'POST':
+    try:
+        _user = User.from_login(
+            request.form['email'],
